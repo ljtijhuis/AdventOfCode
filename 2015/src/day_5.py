e98402fa739194c9e5c43a2f6716fc48fc8b0c0d
Lars Tijhuis <ljtijhuis@gmail.com>
2022-07-16 16:50:30 -0700

    Day 5

---
+++ 2015/src/day_5.py
@@ -0,0 +1,79 @@
+
+from src.file_utils import read_file
+
+'''
+It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+'''
+def has_n_vowels(s, n = 3):
+    found = 0
+    for c in s:
+        if c in 'aeiou':
+            found += 1
+        if found == n:
+            return True
+    return False
+
+'''
+It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+'''
+def has_twice_in_row(s):
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            return True
+    return False
+
+'''
+It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+'''
+def no_forbidden_strings(s):
+    forbidden = ['ab', 'cd', 'pq', 'xy']
+    for f in forbidden:
+        if f in s:
+            return False
+    return True
+
+def is_nice(s):
+    return has_n_vowels(s) and has_twice_in_row(s) and no_forbidden_strings(s)
+
+'''
+It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+'''
+def has_pair(s):
+    pairs = {}
+    for i in range(0, len(s)-1):
+        pair = s[i:i+2]
+        if pair not in pairs:
+            pairs.update({pair: [i]})
+        else:
+            pairs[pair].append(i)
+    
+    for pair, indeces in pairs.items():
+        # just compare the first and last index they occur as they should be furthest apart
+        if len(indeces) > 1 and indeces[len(indeces)-1] - indeces[0] > 1:
+            return True
+    return False
+
+'''
+It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+'''
+def has_repeat(s):
+    for i in range(2, len(s)):
+        if s[i] == s[i-2]:
+            return True
+    return False
+
+def new_is_nice(s):
+    return has_pair(s) and has_repeat(s)
+
+
+input = read_file('/../input/day_5.txt')
+lines = input.split()
+
+total_nice = 0
+total_new_nice = 0
+for line in lines:
+    total_nice += is_nice(line)
+    total_new_nice += new_is_nice(line)
+
+print(total_nice)
+print(total_new_nice)
---
+++ 2015/tests/test_day_5.py
@@ -0,0 +1,102 @@
+import pytest
+from src.day_5 import has_n_vowels, has_twice_in_row, no_forbidden_strings, is_nice, has_pair, has_repeat, new_is_nice
+
+'''
+A nice string is one with all of the following properties:
+It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+'''
+@pytest.mark.parametrize("input, expected_output", [ 
+    ('aei', True),
+    ('xazegov', True),
+    ('aeiouaeiouaeiou', True),
+    ('ai', False),
+    ('xazegv', False),
+    ('mnlklpz', False),
+])
+def test_has_n_vowels(input, expected_output):
+    assert has_n_vowels(input) == expected_output
+
+'''
+It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+'''
+@pytest.mark.parametrize("input, expected_output", [ 
+    ('xx', True),
+    ('abcdde', True),
+    ('aabbccdd', True),
+    ('x', False),
+    ('abcde', False),
+])
+def test_has_twice_in_row(input, expected_output):
+    assert has_twice_in_row(input) == expected_output
+
+'''
+It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+'''
+@pytest.mark.parametrize("input, expected_output", [ 
+    ('ab', False),
+    ('cd', False),
+    ('pq', False),
+    ('xy', False),
+    ('axb', True),
+    ('cxd', True),
+    ('pxq', True),
+    ('xay', True),
+])
+def test_no_forbidden_strings(input, expected_output):
+    assert no_forbidden_strings(input) == expected_output
+
+'''
+For example:
+ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), and none of the disallowed substrings.
+aaa is nice because it has at least three vowels and a double letter, even though the letters used by different rules overlap.
+jchzalrnumimnmhp is naughty because it has no double letter.
+haegwjzuvuyypxyu is naughty because it contains the string xy.
+dvszwmarrgswjxmb is naughty because it contains only one vowel.
+'''
+@pytest.mark.parametrize("input, expected_output", [ 
+    ('ugknbfddgicrmopn', True),
+    ('aaa', True),
+    ('jchzalrnumimnmhp', False),
+    ('haegwjzuvuyypxyu', False),
+    ('dvszwmarrgswjxmb', False),
+])
+def test_is_nice(input, expected_output):
+    assert is_nice(input) == expected_output
+
+'''
+It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+'''
+@pytest.mark.parametrize("input, expected_output", [ 
+    ('xyxy', True),
+    ('aabcdefgaa', True),
+    ('aaa', False),
+    ('aaaa', True),
+])
+def test_has_pair(input, expected_output):
+    assert has_pair(input) == expected_output
+
+'''
+It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+'''
+@pytest.mark.parametrize("input, expected_output", [ 
+    ('xyx', True),
+    ('abcdefeghi', True),
+    ('aaa', True),
+])
+def test_has_repeat(input, expected_output):
+    assert has_repeat(input) == expected_output
+
+'''
+qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+'''
+@pytest.mark.parametrize("input, expected_output", [ 
+    ('qjhvhtzxzqqjkmpb', True),
+    ('xxyxx', True),
+    ('uurcxstgmygtbstg', False),
+    ('ieodomkazucvgmuy', False),
+])
+def test_new_is_nice(input, expected_output):
+    assert new_is_nice(input) == expected_output
