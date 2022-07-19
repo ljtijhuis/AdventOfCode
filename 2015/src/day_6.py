ac8b790f2e8f1dbcdb498cf6679e3cb6b6466811
Lars Tijhuis <ljtijhuis@gmail.com>
2022-07-18 18:23:39 -0700

    Day 6

---
+++ 2015/src/day_6.py
@@ -0,0 +1,107 @@
+
+from src.file_utils import read_file
+
+'''
+Lights in your grid are numbered from 0 to 999 in each direction; the lights at each corner are at 0,0, 0,999, 999,999, and 999,0. 
+The instructions include whether to turn on, turn off, or toggle various inclusive ranges given as coordinate pairs. 
+Each coordinate pair represents opposite corners of a rectangle, inclusive; a coordinate pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3 square. 
+The lights all start turned off.
+'''
+
+class GridInterface:
+    def turn_on(self, x1: int, y1: int, x2: int, y2: int) -> None:
+        pass
+    def turn_off(self, x1: int, y1: int, x2: int, y2: int) -> None:
+        pass
+    def toggle(self, x1: int, y1: int, x2: int, y2: int) -> None:
+        pass
+    def total_count(self) -> int:
+        pass
+
+
+class Grid(GridInterface):
+    def __init__(self) -> None:
+        self.lights_on = set[tuple]()
+
+    def turn_on(self, x1, y1, x2, y2):
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                self.lights_on.add((x, y))
+
+    def turn_off(self, x1, y1, x2, y2):
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                self.lights_on.discard((x, y))
+
+    def toggle(self, x1, y1, x2, y2):
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                if (x, y) in self.lights_on:
+                    self.lights_on.discard((x, y))
+                else:
+                    self.lights_on.add((x, y))
+
+    def total_count(self):
+        return len(self.lights_on)
+
+class BrightnessGrid(GridInterface):
+    def __init__(self) -> None:
+        self.lights_on = dict[tuple, int]()
+
+    def turn_on(self, x1, y1, x2, y2):
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                self.lights_on[(x, y)] = self.lights_on.get((x, y), 0) + 1
+
+    def turn_off(self, x1, y1, x2, y2):
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                self.lights_on[(x, y)] = max(0, self.lights_on.get((x, y), 0) - 1)
+
+    def toggle(self, x1, y1, x2, y2):
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                self.lights_on[(x, y)] = self.lights_on.get((x, y), 0) + 2
+
+    def total_count(self):
+        sum = 0
+        for b in self.lights_on.values():
+            sum += b
+        return sum
+
+class GridController:
+    def __init__(self, grid: GridInterface) -> None:
+        self.grid = grid
+
+    def total_count(self):
+        return self.grid.total_count()
+
+    def run_command(self, cmd: str):
+        words = cmd.split(' ')
+        if cmd.startswith("turn on"):
+            x1, y1 = words[2].split(",")
+            x2, y2 = words[4].split(",")
+            self.grid.turn_on(int(x1), int(y1), int(x2), int(y2))
+        elif cmd.startswith("turn off"):
+            x1, y1 = words[2].split(",")
+            x2, y2 = words[4].split(",")
+            self.grid.turn_off(int(x1), int(y1), int(x2), int(y2))
+        elif cmd.startswith("toggle"):
+            x1, y1 = words[1].split(",")
+            x2, y2 = words[3].split(",")
+            self.grid.toggle(int(x1), int(y1), int(x2), int(y2))
+            
+
+
+input = read_file('/../input/day_6.txt')
+lines = input.splitlines()
+grid = Grid()
+brightness_grid = BrightnessGrid()
+controller = GridController(grid)
+brightness_controller = GridController(brightness_grid)
+for line in lines:
+    controller.run_command(line)
+    brightness_controller.run_command(line)
+
+print(controller.total_count())
+print(brightness_controller.total_count())
---
+++ 2015/tests/test_day_6.py
@@ -0,0 +1,105 @@
+import pytest
+from src.day_6 import Grid, GridController, BrightnessGrid
+
+@pytest.fixture
+def grid():
+    return Grid()
+
+@pytest.fixture
+def brightness_grid():
+    return BrightnessGrid()
+
+@pytest.fixture
+def grid_controller(grid):
+    return GridController(grid)
+
+@pytest.fixture
+def brightness_grid_controller(brightness_grid):
+    return GridController(brightness_grid)
+
+def test_turn_on(grid):
+    grid.turn_on(10, 10, 12, 12) # 9 lights are turned on
+    grid.turn_on(10, 10, 11, 11) # Nothing extra is turned on
+    grid.turn_on(12, 12, 13, 13) # 3 extra lights are turned on
+    grid.turn_on(1, 1, 2, 2) # 4 extra are turned on
+
+    assert grid.total_count() == 16
+
+def test_toggle(grid):
+    grid.toggle(10, 10, 12, 12) # 9 lights are turned on
+    grid.toggle(10, 10, 11, 11) # 4 are turned off
+    grid.toggle(12, 12, 13, 13) # 3 extra lights are turned on, 1 is turned off
+    grid.toggle(1, 1, 2, 2) # 4 extra are turned on
+
+    assert grid.total_count() == 11
+
+def test_turn_off(grid):
+    grid.turn_on(10, 10, 12, 12) # 9 lights are turned on
+    grid.turn_off(10, 10, 11, 11) # 4 are turned off
+    grid.turn_off(12, 12, 13, 13) # 1 is turned off
+    grid.turn_off(1, 1, 2, 2) # Nothing happens
+
+    assert grid.total_count() == 4
+
+'''
+    10  11  12  13  14
+10           x
+11           x
+12   x   x       x   x
+13           x   x   x
+14           x   x   x
+'''
+def test_controller(grid_controller):
+    assert grid_controller.total_count() == 0
+    grid_controller.run_command('turn on 10,10 through 12,12')
+    assert grid_controller.total_count() == 9
+    grid_controller.run_command('turn off 10,10 through 11,11')
+    assert grid_controller.total_count() == 5
+    grid_controller.run_command('toggle 12,12 through 14,14')
+    assert grid_controller.total_count() == 12
+
+
+################################################
+# Now let's do the same for our brightess grid #
+################################################
+def test_brightness_turn_on(brightness_grid):
+    brightness_grid.turn_on(10, 10, 12, 12) # brightness + 9
+    brightness_grid.turn_on(10, 10, 11, 11) # brightness + 4
+    brightness_grid.turn_on(12, 12, 13, 13) # brightness + 4
+    brightness_grid.turn_on(1, 1, 2, 2) # brightness + 4
+
+    assert brightness_grid.total_count() == 21
+
+def test_brightness_toggle(brightness_grid):
+    brightness_grid.toggle(10, 10, 12, 12) # brightness + 18
+    brightness_grid.toggle(10, 10, 11, 11) # brightness + 8
+    brightness_grid.toggle(12, 12, 13, 13) # brightness + 8
+    brightness_grid.toggle(1, 1, 2, 2) # brightness + 8
+
+    assert brightness_grid.total_count() == 42
+
+def test_brightness_turn_off(brightness_grid):
+    brightness_grid.turn_on(10, 10, 12, 12) # brightness + 9
+    brightness_grid.turn_on(10, 10, 12, 12) # brightness + 9
+    brightness_grid.turn_off(10, 10, 11, 11) # brightness - 4
+    brightness_grid.turn_off(10, 10, 11, 11) # brightness - 4
+    brightness_grid.turn_off(10, 10, 11, 11) # nothing happens
+    brightness_grid.turn_off(12, 12, 13, 13) # brightness -1
+    brightness_grid.turn_off(1, 1, 2, 2) # Nothing happens
+
+    assert brightness_grid.total_count() == 9
+
+def test_brightness_controller(brightness_grid_controller):
+    assert brightness_grid_controller.total_count() == 0
+    brightness_grid_controller.run_command('turn on 10,10 through 12,12')
+    assert brightness_grid_controller.total_count() == 9
+    brightness_grid_controller.run_command('turn on 10,10 through 12,12')
+    assert brightness_grid_controller.total_count() == 18
+    brightness_grid_controller.run_command('turn off 10,10 through 11,11')
+    assert brightness_grid_controller.total_count() == 14
+    brightness_grid_controller.run_command('turn off 10,10 through 11,11')
+    assert brightness_grid_controller.total_count() == 10
+    brightness_grid_controller.run_command('turn off 10,10 through 11,11')
+    assert brightness_grid_controller.total_count() == 10
+    brightness_grid_controller.run_command('toggle 12,12 through 14,14')
+    assert brightness_grid_controller.total_count() == 28
